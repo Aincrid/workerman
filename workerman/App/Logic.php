6270c485b7752f7f4be312a91afc15683c5dea77
tao <tao@qq.com>
2018-08-25 18:58:46 +0800

    'im'

---
+++ workerman/App/Logic.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kirito
+ * Date: 2018/8/25
+ * Time: 18:56
+ */
+
+namespace Workerman\App;
+
+/**
+ * Class Logic
+ * @package Workerman\App
+ */
+class Logic
+{
+    /**
+     * @return string  {"data"=>"asdfc"}
+     */
+    public function heartBeat(){
+        echo 'heartBeat';
+        return json_encode(array('data'=>''));
+    }
+}
